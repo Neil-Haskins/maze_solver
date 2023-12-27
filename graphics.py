b779fc2113e9e83a3b7cc854575529758d2134df
Neil Haskins <neil.haskins@studyforge.net>
2023-12-27 10:37:41 -0800

    Make window proper size.

--- graphics.py
+++ graphics.py
@@ -3,8 +3,8 @@ from tkinter import Tk, BOTH, Canvas
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
-        self.__root.title("Root")
-        self.__canvas = Canvas(self.__root)
+        self.__root.title("My maze solver")
+        self.__canvas = Canvas(self.__root, width=width, height=height)
         self.__canvas.pack()
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
